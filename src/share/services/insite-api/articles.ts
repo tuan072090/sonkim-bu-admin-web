35355cb159ee1b6393d815e40050da57e32bff0b
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2021-11-10 14:30:26 +0700

    update api

---
+++ src/share/services/insite-api/articles.ts
@@ -0,0 +1,23 @@
+import FetchDataService from "../fetch"
+
+const ArticleService={
+    getArticles:async(params?:any)=>{
+        try {
+            const {count,articles}=await FetchDataService.GET("/bu-api/articles",params);
+            return {count,articles};
+        } catch (err) {
+            throw err
+        }
+    },
+
+    getArticleDetail:async (articleId:number)=>{
+        try {
+            const {data}=await FetchDataService.GET("/bu-api/articles/"+articleId);
+            return data;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+export default ArticleService;
--- src/share/services/insite-api/index.ts
+++ src/share/services/insite-api/index.ts
@@ -1,10 +1,13 @@
 /* eslint-disable import/no-anonymous-default-export */
 
 import AuthService from "./auth";
-import CategoryServices from "./categories";
-import OrderServices from './orders'
+import ArticleService from "./articles";
+import LoyaltyService from "./loyalty";
+import PromotionService from "./promotions";
+import StoreService from "./stores";
+import VoucherCodeService from "./voucher-codes";
 
 export default {
-    AuthService, CategoryServices, OrderServices
+    AuthService, ArticleService,LoyaltyService,PromotionService,StoreService,VoucherCodeService
 }
 
