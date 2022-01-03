ba661f4462fe12a9b588c86f5dcdbb05c681530c
Tien Tai Nguyen <nguyentientai.3325@gmail.com>
2022-01-03 18:31:12 +0700

    add enter form

--- src/pages/articles/ArticleDetailPage.tsx
+++ src/pages/articles/ArticleDetailPage.tsx
@@ -1,17 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { ChevronLeft } from "react-feather";
 import { useHistory, useParams } from "react-router";
-import { Box, Button, Layout, TextInput } from "../../components";
-import Loader from "../../components/atoms/loader";
+import { Box, Button, Layout, TextInputForm } from "../../components";
 import insiteApi from "../../share/services/insite-api";
 import { DateFormat } from "../../share/utils/formater";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import { useForm } from "react-hook-form";
+
+interface IFormInput {
+    title?: string;
+    slug?: string;
+    body?: string;
+    business_unit?: number;
+    avatar?: number;
+    locale?: string;
+}
 
 const ArticleDetailPage = Layout(() => {
     const params = useParams<{ id: string }>();
     const [articleDetail, setArticleDetail] = useState<any>(null);
     const history = useHistory();
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<IFormInput>();
+
+    const _onSubmit = (data: IFormInput) => {
+        alert(JSON.stringify(data));
+    };
+
     const _fetchArticleDetail = async () => {
         try {
             const data = await insiteApi.ArticleService.getArticleDetail(
@@ -48,87 +67,105 @@ const ArticleDetailPage = Layout(() => {
                                     <p>Trở về</p>
                                 </Button>
                             </div>
-                            <div className="flex flex-col mb-4">
-                                <label className="font-semibold text-base mb-1">
-                                    Tiêu đề:
-                                </label>
-                                <TextInput
-                                    size="small"
-                                    value={articleDetail.Title}
-                                    type="text"
-                                    disabled
-                                    className="w-full"
-                                ></TextInput>
-                            </div>
-                            <div className="flex flex-col mb-4">
-                                <label className="font-semibold text-base mb-1">
-                                    Slug:
-                                </label>
-                                <TextInput
-                                    size="small"
-                                    value={articleDetail.slug}
-                                    type="text"
-                                    disabled
-                                    className="w-100"
-                                ></TextInput>
-                            </div>
-                            <div className="flex flex-col mb-4">
-                                <label className="font-semibold text-base mb-1">
-                                    Hình ảnh:
-                                </label>
-                                <img
-                                    src={
-                                        articleDetail.avatar.formats.thumbnail
-                                            .url
-                                    }
-                                    height={
-                                        articleDetail.avatar.formats.thumbnail
-                                            .height
-                                    }
-                                    width={
-                                        articleDetail.avatar.formats.thumbnail
-                                            .width
-                                    }
-                                />
-                            </div>
-                            <div>
-                                <label className="font-semibold text-base mb-2">
-                                    Nội dung:
-                                </label>
-                                <CKEditor
-                                    editor={ClassicEditor}
-                                    data={articleDetail.body}
-                                    onReady={(editor) => {
-                                        // You can store the "editor" and use when it is needed.
-                                        console.log(
-                                            "Editor is ready to use!",
-                                            editor
-                                        );
-                                    }}
-                                    onChange={(event, editor) => {
-                                        const data = editor.getData();
-                                        console.log({ event, editor, data });
-                                    }}
-                                    onBlur={(event, editor) => {
-                                        console.log("Blur.", editor);
-                                    }}
-                                    onFocus={(event, editor) => {
-                                        console.log("Focus.", editor);
-                                    }}
-                                />
-                            </div>
-                            <div className="flex flex-col mb-4">
-                                <label className="font-semibold text-base mb-1">
-                                    Created at:
-                                </label>
-                                <TextInput
-                                    size="small"
-                                    value={DateFormat(articleDetail.created_at)}
-                                    type="text"
-                                    disabled
-                                    className="w-100"
-                                ></TextInput>
-                            </div>
+                            <form onSubmit={handleSubmit(_onSubmit)}>
+                                <div className="flex flex-col mb-4">
+                                    <label className="font-semibold text-base mb-1">
+                                        Tiêu đề:
+                                    </label>
+                                    <input
+                                        {...register("title")}
+                                        placeholder={articleDetail.title}
+                                        type="text"
+                                        className="w-full py-2 px-3 border rounded-md focus:outline-none focus:ring-2"
+                                    />
+                                    {/* {errors?.title?.type === "required" && (
+                        <p className="italic text-xs text-red-500">
+                            *Không được để trống
+                        </p>
+                    )} */}
+                                </div>
+                                <div className="flex flex-col mb-4">
+                                    <label className="font-semibold text-base mb-1">
+                                        Slug:
+                                    </label>
+                                    <input
+                                        placeholder={articleDetail.slug}
+                                        type="text"
+                                        {...register("slug")}
+                                        className="w-100 py-2 px-3 border rounded-md focus:outline-none focus:ring-2"
+                                    />
+                                </div>
+                                <div className="flex flex-col mb-4">
+                                    <label className="font-semibold text-base mb-1">
+                                        Hình ảnh:
+                                    </label>
+                                    <img
+                                        src={
+                                            articleDetail.avatar.formats
+                                                .thumbnail.url
+                                        }
+                                        height={
+                                            articleDetail.avatar.formats
+                                                .thumbnail.height
+                                        }
+                                        width={
+                                            articleDetail.avatar.formats
+                                                .thumbnail.width
+                                        }
+                                    />
+                                </div>
+                                <div>
+                                    <label className="font-semibold text-base mb-2">
+                                        Nội dung:
+                                    </label>
+                                    <CKEditor
+                                        editor={ClassicEditor}
+                                        data={articleDetail.body}
+                                        onReady={(editor) => {
+                                            // You can store the "editor" and use when it is needed.
+                                            console.log(
+                                                "Editor is ready to use!",
+                                                editor
+                                            );
+                                        }}
+                                        onChange={(event, editor) => {
+                                            const data = editor.getData();
+                                            console.log({
+                                                event,
+                                                editor,
+                                                data,
+                                            });
+                                        }}
+                                        onBlur={(event, editor) => {
+                                            console.log("Blur.", editor);
+                                        }}
+                                        onFocus={(event, editor) => {
+                                            console.log("Focus.", editor);
+                                        }}
+                                    />
+                                </div>
+                                <div className="flex flex-col mb-4">
+                                    <label className="font-semibold text-base mb-1">
+                                        Created at:
+                                    </label>
+                                    <TextInputForm
+                                        size="small"
+                                        value={DateFormat(
+                                            articleDetail.created_at
+                                        )}
+                                        type="text"
+                                        disabled
+                                        className="w-100"
+                                    ></TextInputForm>
+                                </div>
+                                <div className="my-10">
+                                    <input
+                                        type="submit"
+                                        value="Cập nhật"
+                                        className="w-100 mx-auto bg-green-500 rounded-lg border border-green-500 ripple text-white hover:bg-green-700 py-2 px-4 text-base"
+                                    />
+                                </div>
+                            </form>
                         </div>
                     </Box>
                     <Box>
